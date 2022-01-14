0974d4169879b16f05628483aa6d3ac8f07d604a
Oleg Kaliuzhnyi <okaliuzhnyi@mindk.com>
2022-01-14 09:14:23 +0200

    (common):add elevation scroll

--- front/src/components/header/index.tsx
+++ front/src/components/header/index.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { useHistory } from "react-router-dom";
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormGroup from '@material-ui/core/FormGroup';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import Button from '@material-ui/core/Button';
@@ -33,11 +31,28 @@ interface Props {
     handleSignOut: () => void;
 }
 
-const Header: React.FC<Props> = ({ isAuthorised, handleSignOut }) => {
+interface ScrollProps {
+    children: React.ReactElement;
+}
+
+function ElevationScroll(props: ScrollProps) {
+    const { children } = props;
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+    });
+
+    return React.cloneElement(children, {
+        elevation: trigger ? 4 : 0,
+    });
+}
+
+const Header: React.FC<Props> = (props) => {
     const classes = useStyles();
     let history = useHistory();
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
     const open = Boolean(anchorEl);
+    const { isAuthorised, handleSignOut } = props;
 
 
     const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
@@ -63,51 +78,53 @@ const Header: React.FC<Props> = ({ isAuthorised, handleSignOut }) => {
 
     return (
         <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        Photos
-                    </Typography>
-                    {isAuthorised ? (
-                        <div>
-                            <IconButton
-                                aria-label="account of current user"
-                                aria-controls="menu-appbar"
-                                aria-haspopup="true"
-                                onClick={handleMenu}
-                                color="inherit"
-                            >
-                                <AccountCircle />
-                            </IconButton>
-                            <Menu
-                                id="menu-appbar"
-                                anchorEl={anchorEl}
-                                anchorOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                open={open}
-                                onClose={handleClose}
-                            >
-                                <MenuItem onClick={handleProfile}>Profile</MenuItem>
-                                <MenuItem onClick={signOut}>Sign Out</MenuItem>
-                            </Menu>
-                        </div>
-                    ) : (
-                        <Button onClick={handleClose} variant="contained" color="primary">
-                            Sign In
-                        </Button>
-                    )}
-                </Toolbar>
-            </AppBar>
+            <ElevationScroll {...props}>
+                <AppBar position="fixed">
+                    <Toolbar>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" className={classes.title}>
+                            Photos
+                        </Typography>
+                        {isAuthorised ? (
+                            <div>
+                                <IconButton
+                                    aria-label="account of current user"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    onClick={handleMenu}
+                                    color="inherit"
+                                >
+                                    <AccountCircle />
+                                </IconButton>
+                                <Menu
+                                    id="menu-appbar"
+                                    anchorEl={anchorEl}
+                                    anchorOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    open={open}
+                                    onClose={handleClose}
+                                >
+                                    <MenuItem onClick={handleProfile}>Profile</MenuItem>
+                                    <MenuItem onClick={signOut}>Sign Out</MenuItem>
+                                </Menu>
+                            </div>
+                        ) : (
+                            <Button onClick={handleClose} variant="contained" color="primary">
+                                Sign In
+                            </Button>
+                        )}
+                    </Toolbar>
+                </AppBar>
+            </ElevationScroll>
         </div>
     );
 }
--- front/src/pages/profile/index.tsx
+++ front/src/pages/profile/index.tsx
@@ -4,6 +4,14 @@ export default function Profile() {
     return (
         <div className="some-class">
             Profile page
+            {[...new Array(1200)]
+                .map(
+                    () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+                )
+                .join('\n')}
         </div>
     )
 }
